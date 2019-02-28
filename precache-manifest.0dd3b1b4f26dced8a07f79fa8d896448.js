ec2216cf12fd3b3732cbb354e634e31532f9dca8
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2019-02-28 13:13:26 -0600

    Updates

---
+++ precache-manifest.0dd3b1b4f26dced8a07f79fa8d896448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecd3a5fa58602c826838",
+    "url": "/react-restcountries/static/js/runtime~main.ecd3a5fa.js"
+  },
+  {
+    "revision": "ba297e2160a33c8c9170",
+    "url": "/react-restcountries/static/js/main.ba297e21.chunk.js"
+  },
+  {
+    "revision": "b81c284196196ab62d09",
+    "url": "/react-restcountries/static/js/2.b81c2841.chunk.js"
+  },
+  {
+    "revision": "ba297e2160a33c8c9170",
+    "url": "/react-restcountries/static/css/main.0a09bf4e.chunk.css"
+  },
+  {
+    "revision": "b81c284196196ab62d09",
+    "url": "/react-restcountries/static/css/2.5211f276.chunk.css"
+  },
+  {
+    "revision": "2d883b460c3258e8cda032298dec2515",
+    "url": "/react-restcountries/index.html"
+  }
+];
